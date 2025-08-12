aeb8904406370c87c61222ab714db039dc5093f8
malonettoyage <malonettoyage@gmail.com>
2025-08-12 02:57:01 +0200

    Mise à jours MAX
    Ajout banner, notre histoire,video, new img.

--- scroll-fade.js
+++ scroll-fade.js
@@ -25,3 +25,26 @@ document.querySelectorAll('.faq-question').forEach(question => {
       item.classList.toggle('active');
     });
   });
+
+  const video = document.getElementById("portrait-video");
+const toggleBtn = document.getElementById("video-toggle");
+
+let isPlaying = false;
+
+toggleBtn.addEventListener("click", () => {
+  if (isPlaying) {
+    video.pause();
+    toggleBtn.textContent = "▶️ Lire la vidéo";
+  } else {
+    video.play();
+    video.muted = false; // Active le son
+    toggleBtn.textContent = "⏸️ Mettre en pause";
+  }
+  isPlaying = !isPlaying;
+});
+
+// Quand la vidéo se termine → redémarre directement
+video.addEventListener("ended", () => {
+  video.currentTime = 0; // Retour au début
+  video.play();          // Relance
+});
