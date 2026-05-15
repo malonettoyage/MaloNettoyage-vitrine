674cb98597af52fbc769d364e6c49c9c8d16eed2
malonettoyage <malonettoyage@gmail.com>
2026-05-15 15:48:39 +0200

    mise a jours du fontawesom,
    deletes

--- js/autres-articles.js
+++ js/autres-articles.js
@@ -146,7 +146,7 @@
           </div>
           <div class="card-title">${article.titre}</div>
           <p class="card-excerpt">${article.extrait}</p>
-          <span class="card-link">Lire l'article <i class="fa-solid fa-arrow-right"></i></span>
+          <span class="card-link">Lire l'article <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg></span>
         </div>
       </a>`;
   }
--- js/blog-pagination.js
+++ js/blog-pagination.js
@@ -98,7 +98,7 @@
     let html = '';
 
     html += `<button class="pagination-btn pagination-prev" ${pageCourante === 1 ? 'disabled' : ''} data-page="${pageCourante - 1}" aria-label="Page précédente">
-      <i class="fa-solid fa-chevron-left"></i>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="0.625em" height="1em" fill="currentColor" aria-hidden="true"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
     </button>`;
 
     const pages = getPagesAffichees(pageCourante, totalPages);
@@ -111,7 +111,7 @@
     });
 
     html += `<button class="pagination-btn pagination-next" ${pageCourante === totalPages ? 'disabled' : ''} data-page="${pageCourante + 1}" aria-label="Page suivante">
-      <i class="fa-solid fa-chevron-right"></i>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="0.625em" height="1em" fill="currentColor" aria-hidden="true"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
     </button>`;
 
     paginationEl.innerHTML = html;
